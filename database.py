1f08635945682aeb029a88bbb1a9e4ce8ef94215
Pruthvi Kashyap <70802964+RPruthvikashyap@users.noreply.github.com>
2024-11-18 06:11:16 +0530

    Update: description of changes

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:pruthvi123@localhost:5432/ChargePoint"
+# Use the DATABASE_URL from environment variables or fallback to localhost (for local testing)
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:pruthvi123@localhost:5432/ChargePoint")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
